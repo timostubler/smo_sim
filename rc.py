bb5fa02560dd0fdb0e035dbce4e401dbe2eb26a1
timostubler <55137223+timostubler@users.noreply.github.com>
2019-09-25 19:43:09 +0200

    Update rc.py

--- rc.py
+++ rc.py
@@ -11,18 +11,19 @@ Created on Sun Sep 8 20:48:25 2019
 import pandas as pd
 from numpy import linspace as ls
 from scipy.integrate import odeint
-from math import sin
+from math import sin, sqrt
 
-R = 1*10**-1 # Ω
+# T hier nicht mit Zeitkonstante T=RC verwechseln
+R0 = 1*10**-1 # Ω
 C = 1*10**-2 # F
 ue = 5 # V
 uc0 = 0 # V
 i0 = 0 # A
 f = 10 # Hz
-T = 5*R*C # s
-p = T*2 # s
+T = 5*R0*C # s
+p = T*4 # s 
 steps = 200
-ts = ls(0, T*2, steps+1)
+ts = ls(0, T*8, steps+1)
 u0  = uc0
 
 def const(dt):
@@ -30,7 +31,7 @@ def const(dt):
 
 def rect(dt):
     if (dt % p) > p/2:
-        return 0
+        return -ue
     else:
         return ue
     
@@ -39,20 +40,26 @@ def sinus(dt):
 
 signal = rect
 
-def du_dt(u, dt):
+def du_dt1(u, dt):
+    return (signal(dt)-u)/(R0*C)
+
+# Lösung für veränderlichen Widerstandswert
+def du_dt2(u, dt):
+    R = R0/sqrt(abs(signal(dt)-u))
     return (signal(dt)-u)/(R*C)
 
-res = odeint(du_dt, u0, ts)
+res1 = odeint(du_dt1, u0, ts)
+res2 = odeint(du_dt2, u0, ts)
 
 result = pd.DataFrame()
-result['uc'] = res[:,0]
+result['uc1'] = res1[:,0]
+result['uc2'] = res2[:,0]
 result['t'] = ts
 result['ue'] = [signal(dt) for dt in ts]
-ax = result.plot(x='t', y=['ue', 'uc'], grid=True)
+ax = result.plot(x='t', y=['ue', 'uc1','uc2'], grid=True)
 ax.set_title('RC Glied')
 ax.set_xlabel('t [s]')
 print('T:', T)
-
 
 
 
